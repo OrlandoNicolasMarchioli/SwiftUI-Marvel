057088343d5752325849cbfc626d134327d883d1
Nicolas <nico.marchioli@gmail.com>
2024-03-20 11:28:53 -0300

    First project commit

---
+++ Marvel/Marvel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Marvel
+//
+//  Created by Orlando Nicolas Marchioli on 20/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Marvel/Marvel/MarvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelApp.swift
+//  Marvel
+//
+//  Created by Orlando Nicolas Marchioli on 20/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MarvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
