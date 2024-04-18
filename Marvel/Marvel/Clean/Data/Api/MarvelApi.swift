06ec5e7882727d5611695792dc94ccbb9b596b6c
Nicolas <nico.marchioli@gmail.com>
2024-04-18 11:48:39 -0300

    Disengage of code

--- Marvel/Marvel/Clean/Data/Api/MarvelApi.swift
+++ Marvel/Marvel/Clean/Data/Api/MarvelApi.swift
@@ -15,65 +15,71 @@ protocol MarvelApiProtocol {
 
 class MarvelApi: MarvelApiProtocol {
     
-    var characters: [Character] = []
-    var comics: [Comic] = []
+    private var characters: [Character] = []
+    private var comics: [Comic] = []
+    private var baseUrl: String
+    var urlsSession: URLSession
+    
+    init(baseUrl: String = "BASE_URL" ,urlsSession: URLSession = URLSession.shared) {
+        self.baseUrl = baseUrl
+        self.urlsSession = urlsSession
+    }
     
     func fetchCharactersData(completion: @escaping (AllCharactersResponse?, Error?) -> Void)  {
-        let urlString = validateCredentialsAndUrl(credentials: "CREDENTIALS", url: "GET_ALL_CHARACTERS_URL")
+        let urlString = validateCredentialsAndUrl(credentials: "CREDENTIALS", url: baseUrl, path: "characters?")
                 
         guard let url = URL(string: urlString) else {
             print("Error: Invalid URL")
             return
         }
         
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            guard let data = data, error == nil else {
-                print("Error fetching data: \(error?.localizedDescription ?? "Unknown error")")
-                completion(nil, error)
-                return
-            }
-            
-            do {
-                let decoder = JSONDecoder()
-                let response = try decoder.decode(AllCharactersResponse.self, from: data)
-                self.extractAllMoviesFromResponse(response: response)
-                completion(response, nil)
-            } catch {
-                print("Error decoding data: \(error.localizedDescription)")
-                completion(nil, error)
-            }
-        }.resume()
+        performDataTask(urlString: urlString, completion: completion, decodingType: AllCharactersResponse.self, extractResponse: extractAllCharactersFromResponse(response:))
     }
     
     func fetchComicsData(completion: @escaping (ComicDataWrrapper?, Error?) -> Void) {
-        let urlString = validateCredentialsAndUrl(credentials: "CREDENTIALS", url: "GET_ALL_COMICS_URL")
+        let urlString = validateCredentialsAndUrl(credentials: "CREDENTIALS", url: baseUrl, path: "comics?")
                 
         guard let url = URL(string: urlString) else {
             print("Error: Invalid URL")
             return
         }
         
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            guard let data = data, error == nil else {
-                print("Error fetching data: \(error?.localizedDescription ?? "Unknown error")")
-                completion(nil, error)
-                return
-            }
-            
-            do {
-                let decoder = JSONDecoder()
-                let response = try decoder.decode(ComicDataWrrapper.self, from: data)
-                self.extractAllComicsFromResponse(response: response)
-                completion(response, nil)
-            } catch {
-                print("Error decoding data: \(error.localizedDescription)")
-                completion(nil, error)
-            }
-        }.resume()
+        performDataTask(urlString: urlString, completion: completion, decodingType: ComicDataWrrapper.self, extractResponse: extractAllComicsFromResponse(response:))
     }
+    
+    private func performDataTask<T: Decodable>(urlString: String, completion: @escaping (T?, Error?) -> Void, decodingType: T.Type, extractResponse: @escaping (T) -> Void) {
+         guard let url = URL(string: urlString) else {
+             completion(nil, NSError(domain: "Invalid URL", code: -1, userInfo: nil))
+             return
+         }
+         
+         urlsSession.dataTask(with: url) { data, _, error in
+             guard let data = data, error == nil else {
+                 print("Error fetching data: \(error?.localizedDescription ?? "Unknown error")")
+                 DispatchQueue.main.async {
+                     completion(nil, error)
+                 }
+                 return
+             }
+             
+             do {
+                 let decoder = JSONDecoder()
+                 let response = try decoder.decode(decodingType, from: data)
+                 extractResponse(response)
+                 DispatchQueue.main.async {
+                     completion(response, nil)
+                 }
+             } catch {
+                 print("Error decoding data: \(error.localizedDescription)")
+                 DispatchQueue.main.async {
+                     completion(nil, error)
+                 }
+             }
+         }.resume()
+     }
 
     
-    private func extractAllMoviesFromResponse(response : AllCharactersResponse) -> Void{
+    private func extractAllCharactersFromResponse(response : AllCharactersResponse) -> Void{
         self.characters.append(contentsOf: response.data.results)
     }
     
@@ -81,13 +87,13 @@ class MarvelApi: MarvelApiProtocol {
         self.comics.append(contentsOf: response.data?.results ?? [])
     }
     
-    private func validateCredentialsAndUrl(credentials: String, url: String) -> String{
+    private func validateCredentialsAndUrl(credentials: String, url: String, path: String) -> String{
         guard let credentials = NSLocalizedString(credentials, comment: "") as String?,
               let baseURLString = NSLocalizedString(url, comment: "") as String? else {
             print("Error: Could not retrieve localized strings.")
             return ""
         }
-        return baseURLString + credentials
+        return baseURLString + path + credentials
     }
     
     func getAllCharacters() -> [Character]{
