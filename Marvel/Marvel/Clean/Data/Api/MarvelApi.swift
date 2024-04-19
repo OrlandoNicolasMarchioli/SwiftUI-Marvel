57a6a8940734277535f38ca7fb06bcdb816a8131
Nicolas <nico.marchioli@gmail.com>
2024-04-19 16:48:31 -0300

    Adding environment variables to project and cleaning some code

--- Marvel/Marvel/Clean/Data/Api/MarvelApi.swift
+++ Marvel/Marvel/Clean/Data/Api/MarvelApi.swift
@@ -16,6 +16,19 @@ protocol MarvelApiProtocol {
 class MarvelApi: MarvelApiProtocol {
 
     static private var shared: MarvelApi?
+    private var characters: [Character] = []
+    private var comics: [Comic] = []
+    private var baseUrl: String
+    private var apiKey: String
+    private var hash: String
+    private var urlsSession: URLSession
+
+    private init(baseUrl: String, apiKey: String, hash: String, urlsSession: URLSession = URLSession.shared ) {
+        self.baseUrl = baseUrl
+        self.urlsSession = urlsSession
+        self.apiKey = apiKey
+        self.hash = hash
+    }
 
     //TODO : To be improved 
     static func getInstance() -> MarvelApiProtocol{
@@ -23,52 +36,34 @@ class MarvelApi: MarvelApiProtocol {
         return returnShared
       } else {
         let newInstance =
-          MarvelApi(baseUrl: "https://gateway.marvel.com:443/v1/public/",
-                  apiKey: "b209d62336cd3526e0eb13f0bb429891&hash=ac374d862543660c1a035d30825a7a27")
+          MarvelApi(baseUrl: ProcessInfo.processInfo.environment["baseUrl"] ?? "",
+                    apiKey: ProcessInfo.processInfo.environment["apiKey"] ?? "",
+                    hash: ProcessInfo.processInfo.environment["hash"] ?? "")
         shared = newInstance
         return newInstance
       }
     }
-
-
-    private var characters: [Character] = []
-    private var comics: [Comic] = []
-    private var baseUrl: String
-    private var apiKey: String
-    private var urlsSession: URLSession
-
-    private init(baseUrl: String, apiKey: String, urlsSession: URLSession = URLSession.shared ) {
-        self.baseUrl = baseUrl
-        self.urlsSession = urlsSession
-        self.apiKey = apiKey
-    }
     
     func fetchCharactersData(completion: @escaping (AllCharactersResponse?, Error?) -> Void)  {
-        guard let url = absoluteURLFactory(host: baseUrl, path: "characters", apiKey: apiKey) else {
+        guard let url = absoluteURLFactory(host: baseUrl, path: "characters", apiKey: apiKey, hash: hash) else {
             print("Error: Invalid URL")
             return
         }
-        performDataTask(urlString: url.absoluteString, completion: completion, decodingType: AllCharactersResponse.self, extractResponse: extractAllCharactersFromResponse(response:))
+        performDataTask(urlString: url, completion: completion, decodingType: AllCharactersResponse.self, extractResponse: extractAllCharactersFromResponse(response:))
     }
     
     func fetchComicsData(completion: @escaping (ComicDataWrrapper?, Error?) -> Void) {
-        let urlString = validateCredentialsAndUrl(credentials: "CREDENTIALS", url: baseUrl, path: "comics?")
-                
-        guard let url = URL(string: urlString) else {
+        guard let url = absoluteURLFactory(host: baseUrl, path: "comics", apiKey: apiKey, hash: hash) else {
             print("Error: Invalid URL")
             return
         }
         
-        performDataTask(urlString: urlString, completion: completion, decodingType: ComicDataWrrapper.self, extractResponse: extractAllComicsFromResponse(response:))
+        performDataTask(urlString: url, completion: completion, decodingType: ComicDataWrrapper.self, extractResponse: extractAllComicsFromResponse(response:))
     }
     
-    private func performDataTask<T: Decodable>(urlString: String, completion: @escaping (T?, Error?) -> Void, decodingType: T.Type, extractResponse: @escaping (T) -> Void) {
-         guard let url = URL(string: urlString) else {
-             completion(nil, NSError(domain: "Invalid URL", code: -1, userInfo: nil))
-             return
-         }
+    private func performDataTask<T: Decodable>(urlString: URL, completion: @escaping (T?, Error?) -> Void, decodingType: T.Type, extractResponse: @escaping (T) -> Void) {
          
-         urlsSession.dataTask(with: url) { data, _, error in
+        urlsSession.dataTask(with: urlString) { data, _, error in
              guard let data = data, error == nil else {
                  print("Error fetching data: \(error?.localizedDescription ?? "Unknown error")")
                  DispatchQueue.main.async {
@@ -93,7 +88,6 @@ class MarvelApi: MarvelApiProtocol {
          }.resume()
      }
   
-    
     private func extractAllCharactersFromResponse(response : AllCharactersResponse) -> Void{
         self.characters.append(contentsOf: response.data.results)
     }
@@ -102,19 +96,16 @@ class MarvelApi: MarvelApiProtocol {
         self.comics.append(contentsOf: response.data?.results ?? [])
     }
     
-    private func validateCredentialsAndUrl(credentials: String, url: String, path: String) -> String {
-        return url + path + credentials
-    }
-    
     func getAllCharacters() -> [Character]{
         return self.characters
     }
   
-    private func absoluteURLFactory(host: String, path: String, apiKey: String) -> URL?{
+    private func absoluteURLFactory(host: String, path: String, apiKey: String, hash: String) -> URL?{
       var hostUrl = URL(string: host)
-      hostUrl?.append(path: "characters")
+      hostUrl?.append(path: path)
       hostUrl?.append(queryItems: [URLQueryItem(name: "ts", value: "1")])
       hostUrl?.append(queryItems: [URLQueryItem(name: "apikey", value: apiKey)])
+      hostUrl?.append(queryItems: [URLQueryItem(name: "hash", value: hash)])
       return hostUrl
     }
 }
